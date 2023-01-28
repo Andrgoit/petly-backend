0af1433bb3094bac36d34c47ecb19a8f0902f514
Sergey Filatov <94907830+Filatov44@users.noreply.github.com>
2023-01-28 20:52:50 +0300

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
 const HttpError = require("./httpError");
 const handleMongooseError = require("./handleMongooseError");
-const controllerWrappers = require("./controlllerWrappers");
+const controllerWrappers = require("./controllerWrappers");
 const createToken = require("./createToken");
 const getFileUrl = require("./getFileUrl");
 const removeFile = require("./removeFile");
